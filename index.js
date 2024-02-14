5397bcfcc60a4f1181f3e7e4729863f7d94a8fd1
nabin <aanabin18@gmail.com>
2024-02-14 17:40:25 +0600

    post method complete

--- index.js
+++ index.js
@@ -17,10 +17,6 @@ app.use(express.json());
 
 
 
-
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cqhoysn.mongodb.net/?retryWrites=true&w=majority`;
 
 
@@ -36,6 +32,45 @@ async function run() {
   try {
   
     await client.connect();
+
+
+    const productCollection = client.db('productDB').collection('products')
+
+
+    app.get('/products' , async (req, res) =>{
+      const cursor = productCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.post("/products", async (req, res) => {
+      const newProducts = req.body;
+      const result = await productCollection.insertOne(newProducts);
+      res.send(result);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
    
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
